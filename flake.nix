ce27c0909d66d62f4d5ba330f1d61fb1201298a9
Peter Cummuskey <peterc@aetheric.co.nz>
2025-11-30 06:03:53 +0000

    Fixing compile error.

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
 	inputs = {
 
 		nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
-		inputs.systems.url = "github:nix-systems/default";
+		systems.url = "github:nix-systems/default";
 
 		flake-utils = {
 			url = "github:numtide/flake-utils";
